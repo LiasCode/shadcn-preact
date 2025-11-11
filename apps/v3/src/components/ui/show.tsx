5688d32649af6934b644ab7ec7e0ac56667c259d
LiasCode <liasraul00@gmail.com>
2025-11-11 02:42:42 -0500

    feat: update tsconfig and start usign bun

--- apps/v3/src/components/ui/show.tsx
+++
@@ -1,8 +0,0 @@
-import { useMemo, type PropsWithChildren } from "preact/compat";
-
-export type ShowProps = PropsWithChildren<{ when: boolean }>;
-
-export function Show(props: ShowProps) {
-  const show_children = useMemo(() => Boolean(props.when) === true, [props.when]);
-  return show_children ? props.children : null;
-}
--- apps/v4/src/components/ui/share/debounce.ts
+++
@@ -1,9 +0,0 @@
-export function debounce(fn: (...args: any[]) => any, delay: number) {
-  let timeout: NodeJS.Timeout;
-  return (...args: any[]) => {
-    clearTimeout(timeout);
-    timeout = setTimeout(() => {
-      fn(...args);
-    }, delay);
-  };
-}
