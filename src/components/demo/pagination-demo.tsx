be17a4f29f0001a3c27cfd9307879ae1712f4de9
LiasCode <liasraul00@gmail.com>
2026-04-06 15:52:50 +0200

    feat: update pagination component

--- src/components/demo/pagination-demo.tsx
+++ src/components/demo/pagination-demo.tsx
@@ -7,10 +7,12 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from "@/components/ui/pagination";
+import { Field, FieldLabel } from "@ui/field";
+import { NativeSelect, NativeSelectOption } from "@ui/native-select";
 
 export function PaginationDemo() {
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex flex-col items-start justify-start gap-8">
       <Pagination className="mx-0 w-fit">
         <PaginationContent>
           <PaginationItem>
@@ -35,6 +37,62 @@ export function PaginationDemo() {
           </PaginationItem>
           <PaginationItem>
             <PaginationNext href="/#" />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+
+      <Pagination className="mx-0 w-fit">
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationLink href="#">1</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink
+              href="#"
+              isActive
+            >
+              2
+            </PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#">3</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#">4</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#">5</PaginationLink>
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+
+      <PaginationIconsOnly />
+    </div>
+  );
+}
+
+export function PaginationIconsOnly() {
+  return (
+    <div className="flex items-center justify-between gap-4">
+      <Field
+        orientation="horizontal"
+        className="w-fit"
+      >
+        <FieldLabel htmlFor="select-rows-per-page">Rows per page</FieldLabel>
+        <NativeSelect defaultValue="25">
+          <NativeSelectOption value="10">10</NativeSelectOption>
+          <NativeSelectOption value="25">25</NativeSelectOption>
+          <NativeSelectOption value="50">50</NativeSelectOption>
+          <NativeSelectOption value="100">100</NativeSelectOption>
+        </NativeSelect>
+      </Field>
+      <Pagination className="mx-0 w-auto">
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious href="#" />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationNext href="#" />
           </PaginationItem>
         </PaginationContent>
       </Pagination>
--- src/components/ui/pagination.tsx
+++ src/components/ui/pagination.tsx
@@ -1,4 +1,4 @@
-import { type Button, buttonVariants } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import { ChevronLeftIcon, ChevronRightIcon, MoreHorizontalIcon } from "lucide-preact";
 import type { ComponentProps } from "preact";
 import { cn } from "./share/cn";
@@ -18,7 +18,7 @@ function PaginationContent({ className, ...props }: ComponentProps<"ul">) {
   return (
     <ul
       data-slot="pagination-content"
-      className={cn("flex flex-row items-center gap-1", className)}
+      className={cn("flex items-center gap-0.5", className)}
       {...props}
     />
   );
@@ -40,46 +40,60 @@ type PaginationLinkProps = {
 
 function PaginationLink({ className, isActive, size = "icon", ...props }: PaginationLinkProps) {
   return (
-    <a
-      aria-current={isActive ? "page" : undefined}
-      data-slot="pagination-link"
-      data-active={isActive}
-      className={cn(
-        buttonVariants({
-          variant: isActive ? "outline" : "ghost",
-          size,
-        }),
-        className
-      )}
-      {...props}
-    />
+    <Button
+      asChild
+      variant={isActive ? "outline" : "ghost"}
+      size={size}
+      className={cn(className)}
+    >
+      <a
+        aria-current={isActive ? "page" : undefined}
+        data-slot="pagination-link"
+        data-active={isActive}
+        {...props}
+      />
+    </Button>
   );
 }
 
-function PaginationPrevious({ className, ...props }: ComponentProps<typeof PaginationLink>) {
+function PaginationPrevious({
+  className,
+  text = "Previous",
+  ...props
+}: ComponentProps<typeof PaginationLink> & { text?: string }) {
   return (
     <PaginationLink
       aria-label="Go to previous page"
       size="default"
-      className={cn("gap-1 px-2.5 sm:pl-2.5", className)}
+      className={cn("pl-1.5!", className)}
       {...props}
     >
-      <ChevronLeftIcon />
-      <span className="hidden sm:block">Previous</span>
+      <ChevronLeftIcon
+        data-icon="inline-start"
+        className="cn-rtl-flip"
+      />
+      <span className="hidden sm:block">{text}</span>
     </PaginationLink>
   );
 }
 
-function PaginationNext({ className, ...props }: ComponentProps<typeof PaginationLink>) {
+function PaginationNext({
+  className,
+  text = "Next",
+  ...props
+}: ComponentProps<typeof PaginationLink> & { text?: string }) {
   return (
     <PaginationLink
       aria-label="Go to next page"
       size="default"
-      className={cn("gap-1 px-2.5 sm:pr-2.5", className)}
+      className={cn("pr-1.5!", className)}
       {...props}
     >
-      <span className="hidden sm:block">Next</span>
-      <ChevronRightIcon />
+      <span className="hidden sm:block">{text}</span>
+      <ChevronRightIcon
+        data-icon="inline-end"
+        className="cn-rtl-flip"
+      />
     </PaginationLink>
   );
 }
@@ -89,10 +103,10 @@ function PaginationEllipsis({ className, ...props }: ComponentProps<"span">) {
     <span
       aria-hidden
       data-slot="pagination-ellipsis"
-      className={cn("flex size-9 items-center justify-center", className)}
+      className={cn("flex size-8 items-center justify-center [&_svg:not([class*='size-'])]:size-4", className)}
       {...props}
     >
-      <MoreHorizontalIcon className="size-4" />
+      <MoreHorizontalIcon />
       <span className="sr-only">More pages</span>
     </span>
   );
